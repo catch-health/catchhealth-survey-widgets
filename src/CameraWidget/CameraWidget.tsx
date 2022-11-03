34a78e8d0c6abb7f19708934f7066ec6d819b171
Erid <erid14@gmail.com>
2022-11-03 16:13:48 -0600

    Use mobile native camera
    And default to mirrored on desktop.

--- src/CameraWidget/CameraWidget.tsx
+++ src/CameraWidget/CameraWidget.tsx
@@ -20,6 +20,7 @@ export const CameraWidget = ({
 }: Props) => {
 	const [cameraOpen, setCameraOpen] = useState(false);
 	const [photos, setPhotos] = useState<File[]>([]);
+	const cameraInput = React.useRef<HTMLInputElement>(null);
 
 	const handleTakePhoto = (dataUri: string) => {
 		const file = dataUriToFile(dataUri, `${fileName}_${Date.now()}.png`);
@@ -28,9 +29,25 @@ export const CameraWidget = ({
 		setCameraOpen(false);
 	};
 
+	const handleMobileTakePhoto = (e: React.ChangeEvent<HTMLInputElement>) => {
+		if (e.target.files && e.target.files.length > 0) {
+			const file = e.target.files[0];
+			onAddPhoto(file);
+			setPhotos((prevValue) => [...prevValue, file]);
+		}
+	};
+
 	const handleRemovePhoto = (index: number) => {
 		onRemovePhoto(index);
 		setPhotos((prevValue) => prevValue.filter((_, i) => i !== index));
+	};
+
+	const handleOpenCamera = () => {
+		if (/Android|iPhone/i.test(navigator.userAgent)) {
+			cameraInput.current?.click();
+		} else {
+			setCameraOpen(true);
+		}
 	};
 
 	const hasPhotos = photos.length > 0;
@@ -54,11 +71,18 @@ export const CameraWidget = ({
 				<button
 					type="button"
 					className="relative block w-full rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-[color:var(--primary)] group"
-					onClick={() => setCameraOpen(true)}
+					onClick={handleOpenCamera}
 				>
 					<Icon
 						icon={camera}
 						className="mx-auto h-12 w-12 group-hover:text-[var(--primary)]"
+					/>
+					<input
+						ref={cameraInput}
+						type="file"
+						accept="image/*"
+						className="hidden"
+						onChange={handleMobileTakePhoto}
 					/>
 					<span className="mt-2 block text-sm font-medium group-hover:text-[var(--primary)]">
 						Take a photo
