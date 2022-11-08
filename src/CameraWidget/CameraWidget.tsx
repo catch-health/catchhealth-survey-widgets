34b3b11e8c67cbdee9ce46b189526d6d20fa6424
Elías Balioceda Fernández <erid14@gmail.com>
2022-11-08 16:48:27 -0600

    Show loading spinner when uploading photo (#3)

--- src/CameraWidget/CameraWidget.tsx
+++ src/CameraWidget/CameraWidget.tsx
@@ -9,7 +9,7 @@ type Props = {
 	allowMultiplePhotos?: boolean;
 	fileName?: string;
 	initialValue?: File[];
-	onAddPhoto: (file: File) => void;
+	onAddPhoto: (file: File) => Promise<void>;
 	onRemovePhoto: (index: number) => void;
 };
 
@@ -22,13 +22,17 @@ export const CameraWidget = ({
 }: Props) => {
 	const [cameraOpen, setCameraOpen] = useState(false);
 	const [photos, setPhotos] = useState<File[]>(initialValue);
+	const [uploading, setUploading] = useState(false);
 	const cameraInput = React.useRef<HTMLInputElement>(null);
 
-	const handleTakePhoto = (dataUri: string) => {
+	const handleTakePhoto = async (dataUri: string) => {
 		const file = dataUriToFile(dataUri, `${fileName}_${Date.now()}.png`);
-		onAddPhoto(file);
-		setPhotos((prevValue) => [...prevValue, file]);
+
 		setCameraOpen(false);
+		setUploading(true);
+		await onAddPhoto(file);
+		setUploading(false);
+		setPhotos((prevValue) => [...prevValue, file]);
 	};
 
 	const handleMobileTakePhoto = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -55,12 +59,41 @@ export const CameraWidget = ({
 	const hasPhotos = photos.length > 0;
 
 	return (
-		<>
+		<div className="relative">
 			<CameraModal
 				onClose={() => setCameraOpen(false)}
 				onTakePhoto={handleTakePhoto}
 				open={cameraOpen}
 			/>
+
+			{uploading && (
+				<>
+					<div className="z-10 absolute left-0 top-0 bg-gray-400 opacity-60 w-full h-full" />
+					<div className="z-10 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 inline-flex items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md bg-[color:var(--background)] transition ease-in-out duration-150 cursor-not-allowed">
+						<svg
+							className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+						>
+							<circle
+								className="opacity-25"
+								cx="12"
+								cy="12"
+								r="10"
+								stroke="currentColor"
+								stroke-width="4"
+							></circle>
+							<path
+								className="opacity-75"
+								fill="currentColor"
+								d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+							></path>
+						</svg>
+						Uploading...
+					</div>
+				</>
+			)}
 
 			{hasPhotos ? (
 				<PhotoCarousel
@@ -91,6 +124,6 @@ export const CameraWidget = ({
 					</span>
 				</button>
 			)}
-		</>
+		</div>
 	);
 };
